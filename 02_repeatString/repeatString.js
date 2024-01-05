d3cdad8b6f879bc288ca9b1d223c220c0bdd76c1
Shreyas Pareek <shreyaspareek3101@gmail.com>
2024-01-05 16:26:11 +0530

    Update repeatString.js and repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    let str = "";
+    for (let i = 1; i <= num; i++) {
+      str += string;
+    }
+    return str;
+  }
 };
 
 // Do not edit below this line
